20d89394d59b2f0d6becdc1ce43d70d67b40dbc5
Vasilii Karpenko <invweb@gmail.com>
2021-08-27 09:38:04 +0300

    init
    init

---
+++ app/src/main/java/com/dagger/hilt/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.dagger.hilt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+import javax.inject.Named
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    @Inject
+    lateinit var testClass: TestClass
+    @Inject
+    @field:Named("qwe")
+    lateinit var testStringQwe : String
+    @Inject
+    @field:Named("asd")
+    lateinit var testStringAsd : String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        testClass.method()
+    }
+}
